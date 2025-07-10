55e17ed75d6992e79fa3c70fb6e7a6569454b774
Ludson Almeida <ludsonalmeida5@gmail.com>
2025-07-10 16:03:58 -0300

    Primeiro commit

---
+++ checar_pacotes.py
@@ -0,0 +1,10 @@
+# import igraph
+# print("Core igraph version:", igraph.__version__)
+# print("Python binding (binding) version:", igraph.__binding_version__)
+
+
+
+import igraph
+print(igraph.__version__)       # deve mostrar 0.10.x
+help(igraph.Graph.from_networkx)
+# help(igraph.Graph._construct_graph_from_networkx)
